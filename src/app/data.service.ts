af489260d5bf6e1d6a4557704f412b4351921ae8
Jeremy Aguillon <agui1@umbc.edu>
2019-04-07 14:47:44 -0400

    updates

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ITeam } from './entities/ITeam';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,10 @@ export class DataService {
   constructor(private http: HttpClient) { }
 
   getTeams(){
-    return this.http.get('../assets/json/records.json')
+    return this.http.get<ITeam[]>('../assets/json/teams.json')
   }
 
   getTeam(teamId){
-    return this.http.get('../assets/json/records.json?id='+teamId)
+    return this.http.get('../assets/json/'+teamId+'.json')
   }
 }
